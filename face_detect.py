1642be6229fae25c8b25c6f0ca21d13b212858d9
abha-m <abhamutalik@gmail.com>
2019-03-02 01:31:56 +0530

    added face_detect.py, modified test.py

---
+++ face_detect.py
@@ -0,0 +1,39 @@
+# Reference: https://codereview.stackexchange.com/questions/156736/cropping-faces-from-images-in-a-directory
+
+import cv2
+import glob
+import os
+
+def facechop(image,imgname):
+    facedata = "haarcascade_frontalface_alt.xml"
+    cascade = cv2.CascadeClassifier(facedata)
+
+    img = cv2.imread(image)
+
+    minisize = (img.shape[1],img.shape[0])
+    miniframe = cv2.resize(img, minisize)
+
+    faces = cascade.detectMultiScale(miniframe)
+
+    for f in faces:
+        x, y, w, h = [ v for v in f ]
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255))
+
+        sub_face = img[y:y+h, x:x+w]
+        face_file_name = "/home/abha/Celeb_faces/train/all_face_detect_cropped/" + imgname + "_" + str(y) + ".jpg"
+        cv2.imwrite(face_file_name, sub_face)
+
+    # cv2.imshow(image, img)
+    # cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return
+
+# if __name__  '__main__':
+path = "/home/abha/Celeb_faces/train/all"
+images = glob.glob(path + "/*")
+for imgpath in images[0:]:
+    imagename = os.path.basename(imgpath)
+    imgname = os.path.splitext(imagename)[0]
+    facechop(imgpath, imgname)
+    print(imgname,"exported")
--- test.py
+++ test.py
@@ -6,16 +6,25 @@ import os
 
 model = VGG16(weights='imagenet', include_top=False)
 model.summary()
-vgg16_feature_list = []
-folder_path = '/home/harsh/Projects/data/All'
+# vgg16_feature_list = []
+file_name_and_feature = dict()
+folder_path = '/home/abha/Celeb_faces/train/all_face_detect_cropped'
+
 for filename in os.listdir(folder_path):
+    feature_list_of_this_image = []
+    file_name = folder_path + '/' + filename
+
     img = image.load_img(os.path.join(folder_path, filename), target_size=(224, 224))
     img_data = image.img_to_array(img)
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data)
-
     vgg16_feature = model.predict(img_data)
     vgg16_feature_np = np.array(vgg16_feature)
-    vgg16_feature_list.append(vgg16_feature_np.flatten())
+    # vgg16_feature_list.append(vgg16_feature_np.flatten())
+    feature_list_of_this_image.append(vgg16_feature_np.flatten())
+    file_name_and_feature[file_name] = feature_list_of_this_image
 
-print(vgg16_feature_list[0].shape)
+
+# print(vgg16_feature_list[0].shape)
+test_filepath = '/home/abha/Celeb_faces/train/all_face_detect_cropped/httpftqncomymusicLxZeltonjohnjpg_30.jpg'
+print(file_name_and_feature[test_filepath][0].shape)
